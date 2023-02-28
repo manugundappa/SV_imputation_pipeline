746003c9a7d9c57b16040d44ec5d9987d8a71394
manugundappa <88680857+manugundappa@users.noreply.github.com>
2023-02-28 11:37:24 +0000

    Add files via upload

---
+++ In_panel_samples/No_SV_GL_scripts/submitjobs_phase.sh
@@ -0,0 +1,21 @@
+samples=("Enni_12_0008" "Enni_12_0051" "Enni_12_0085" "Enni_12_0092" "Enni_12_0102" "Enni_12_0032" "Enni_12_0064" "Enni_12_0091" "Enni_12_0094" "Enni_12_0105")
+
+chr=("ssa01" "ssa02" "ssa03" "ssa04" "ssa05" "ssa06" "ssa07" "ssa08" "ssa09" "ssa10" "ssa11" "ssa12" "ssa13" "ssa14" "ssa15" "ssa16" "ssa17" "ssa18" "ssa19" "ssa20" "ssa21" "ssa22" "ssa23" "ssa24" "ssa25" "ssa26" "ssa27" "ssa28" "ssa29")
+
+
+#loop through samples and chromosomes
+for (( a = 0; a < 10; a++ ))
+do
+	for (( b = 0; b < 29; b++ )) 
+	do
+		while IFS="" read -r LINE || [ -n "$LINE" ];
+		do
+			printf -v ID "%02d" $(echo $LINE | cut -d" " -f1)
+			IRG=$(echo $LINE | cut -d" " -f3)
+			ORG=$(echo $LINE | cut -d" " -f4)
+			echo "${samples[$a]} ${chr[$b]} ${IRG} ${ORG} ${ID}"
+			qsub submitPhase.sh ${samples[$a]} ${chr[$b]} ${IRG} ${ORG} ${ID}
+		done < chunks.${samples[$a]}.${chr[$b]}.txt
+	done
+done
+
