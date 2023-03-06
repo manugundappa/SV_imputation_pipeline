c2d915297d7fff9d7c6faad5ce2dd5128e077d43
manugundappa <88680857+manugundappa@users.noreply.github.com>
2023-03-06 10:55:46 +0000

    Add files via upload

---
+++ In_panel_samples/With_SV_GL_scripts/submitjobs_inpanel_genotype.sh
@@ -0,0 +1,12 @@
+samples=("Enni_12_0008" "Enni_12_0051" "Enni_12_0085" "Enni_12_0092" "Enni_12_0102" "Enni_12_0032" "Enni_12_0064" "Enni_12_0091" "Enni_12_0094" "Enni_12_0105")
+
+chr=("ssa01" "ssa02" "ssa03" "ssa04" "ssa05" "ssa06" "ssa07" "ssa08" "ssa09" "ssa10" "ssa11" "ssa12" "ssa13" "ssa14" "ssa15" "ssa16" "ssa17" "ssa18" "ssa19" "ssa20" "ssa21" "ssa22" "ssa23" "ssa24" "ssa25" "ssa26" "ssa27" "ssa28" "ssa29")
+
+for (( a = 0; a < 20; a++ ))
+do
+        for (( b = 0; b < 29; b++ ))
+        do
+                echo "${samples[$a]} ${chr[$b]} $1"
+		qsub submit_genotype.sh ${samples[$a]} ${chr[$b]} $1
+        done
+done
